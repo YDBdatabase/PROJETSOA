82222bcaf0d39b73d05a5e63bbeb6688be2234ca
Priority <gwenole-lv@live.fr>
2021-04-16 11:06:48 +0200

    front update js

--- front/script.js
+++ front/script.js
@@ -1,10 +1,52 @@
 function signIn(){
-    localStorage.setItem("username","test");
-    window.location.reload(false);
+    let data = $("form").serialize()
+    
+    let req = $.ajax({
+        url: 'http://'+dockerIP+':5000/users/connect',
+        type: 'POST',
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        dataType: "json",
+        success: [function(){
+            localStorage.setItem("username","test");
+            window.location.reload(false);
+        }],
+        error: [function () {
+            alert('Error on login');
+        }]
+    });
+
+    req.then(function(response) {
+        console.log(response)
+    }, function(xhr) {
+        console.error('failed to fetch xhr', xhr)
+    })
+    
+    
 }
 
 function signUp(){
-    localStorage.setItem("username","test");
+    let data = $("form").serialize()
+    
+    let req = $.ajax({
+        url: 'http://'+dockerIP+':5000/users/register',
+        type: 'POST',
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        dataType: "json",
+        success: [function(){
+            alert('User Created');
+        }],
+        error: [function () {
+            alert('Error on user creation');
+        }]
+    });
+
+    req.then(function(response) {
+        console.log(response)
+    }, function(xhr) {
+        console.error('failed to fetch xhr', xhr)
+    })
 }
 
 function logOut(){
