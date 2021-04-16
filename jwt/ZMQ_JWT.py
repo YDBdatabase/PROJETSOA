ba395a49c663dae5f542e5ca8f01c7540c568a56
Bigest <bigest28@gmail.com>
2021-04-16 12:56:24 +0200

    JWT working

--- jwt/ZMQ_JWT.py
+++ jwt/ZMQ_JWT.py
@@ -1,6 +1,8 @@
 import jwt
 import time
 import zmq
+import json
+from io import StringIO
 import threading
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
@@ -11,43 +13,14 @@ f = open('mykey.pem','wb')
 f.write(key.export_key('PEM'))
 f.close()
 """
-f = open('mykey.pem','r')
-key = RSA.import_key(f.read())
-"""
-public_key = key.publickey().export_key()
-file_out = open("receiver.pem", "wb")
-file_out.write(public_key)
-file_out.close()
 
-context = zmq.Context()
-send_socket = context.socket(zmq.PUSH)
-send_socket.bind('tcp://*:5556')
-
-def print_incoming_messages():
-    recv_socket = context.socket(zmq.PULL)
-    recv_socket.bind('tcp://*:5557')
-    while True:
-        msg = recv_socket.recv_string()
-        print(f'Message from client: {msg}')
-
-# Print incoming messages in background
-recv_thread = threading.Thread(target=print_incoming_messages)
-recv_thread.start()
-
-while True:
-    msg = input('Message to send: ')
-    send_socket.send_string(msg)
-"""
-encoded_jwt = jwt.encode({'some': 'payload'}, 'aogkrejgi2GA651g5&4dgth6781zlhafi12gri93p3uDNCe', algorithm='HS256')
-#print(encoded_jwt) 
-encoded_jwtsplited=encoded_jwt.split(".")
 
 def encrypt_data(data):
     
     session_key = get_random_bytes(16)
-
+    recipient_key = RSA.import_key(open("receiver.pem").read())
     # Encrypt the session key with the public RSA key
-    cipher_rsa = PKCS1_OAEP.new(key)
+    cipher_rsa = PKCS1_OAEP.new(recipient_key)
     enc_session_key = cipher_rsa.encrypt(session_key)
 
     # Encrypt the data with the AES session key
@@ -56,22 +29,48 @@ def encrypt_data(data):
     ciphertext, tag = cipher_aes.encrypt_and_digest(data.encode("utf-8"))
     return ciphertext,tag,cipher_aes.nonce,enc_session_key
 
-ciphertext,tag,nonce,enc_session_key=encrypt_data(encoded_jwtsplited[0])
+def sendJWTToken(secret):
+    encoded_jwt = jwt.encode(secret, 'aogkrejgi2GA651g5&4dgth6781zlhafi12gri93p3uDNCe', algorithm='HS256')
+    #print(encoded_jwt) 
+    encoded_jwtsplited=encoded_jwt.split(".")
 
-def decrypt(data,key,tag,nonce,enc_session_key):
-    # Decrypt the session key with the private RSA key
-    recipient_key = RSA.import_key(open("receiver.pem").read())
-    cipher_rsa = PKCS1_OAEP.new(recipient_key)
-    session_key = cipher_rsa.decrypt(enc_session_key)
+    ciphertext,tag,nonce,enc_session_key=encrypt_data(encoded_jwtsplited[0])
+    #print(ciphertext)
+    #print(decrypt(ciphertext, key, tag, nonce, enc_session_key))
 
-    # Decrypt the data with the AES session key
-    cipher_aes_dec = AES.new(session_key, AES.MODE_EAX, nonce)
-    data = cipher_aes_dec.decrypt_and_verify(data, tag)
-    return data.decode("utf-8")
+    jwtchiffre=[]
 
-for jwtencoded in encoded_jwtsplited:
-    print(jwtencoded)
-    jwtchiffre,tag,nonce,enc_session_key=encrypt_data(jwtencoded)
-    print(jwtchiffre)
-    print(decrypt(jwtchiffre,key,tag,nonce,enc_session_key))
-jwt=jwt.decode(encoded_jwt, 'aogkrejgi2GA651g5&4dgth6781zlhafi12gri93p3uDNCe', algorithms=['HS256'])
+    for i in range(len(encoded_jwtsplited)):
+        res,tag,nonce,enc_key=encrypt_data(encoded_jwtsplited[i])
+        #print(res,tag,nonce,enc_key)
+        jwtchiffre.append([res.decode('ISO-8859-1') ,tag.decode('ISO-8859-1'),nonce.decode('ISO-8859-1'),enc_key.decode('ISO-8859-1')])
+
+    return(jwtchiffre) #SEND MESSAGE    
+
+
+context = zmq.Context()
+send_socket = context.socket(zmq.PUSH)
+send_socket.bind('tcp://*:5576')
+
+recv_socket = context.socket(zmq.PULL)
+recv_socket.bind('tcp://*:5577')
+while True:
+    msg = recv_socket.recv_string()
+    print(f'Message from client: {msg}')
+    msgToken = sendJWTToken(json.loads(msg))#{'some': 'payload'}
+    messagesplit=""
+    for i in range(len(msgToken)):
+        msginter=""
+        for j in range(len(msgToken[i])):
+            msginter+=str(msgToken[i][j])+"^^^"
+        messagesplit+=str(msginter)+"***"
+    print("Sending...")
+    send_socket.send_string(str(messagesplit))
+
+    
+
+
+    #Decode JWT Token
+    """encoded_chiffre_jwt=decryptJWTToken(msgsplit)
+    jwtrest=jwt.decode(encoded_chiffre_jwt, 'aogkrejgi2GA651g5&4dgth6781zlhafi12gri93p3uDNCe', algorithms=['HS256'])
+    print("jwt= ",jwtrest)"""
--- jwt/ZMQtestClient.py
+++ jwt/ZMQtestClient.py
@@ -14,16 +14,47 @@ def decrypt(data,key,tag,nonce,enc_session_key):
     data = cipher_aes_dec.decrypt_and_verify(data, tag)
     return data.decode("utf-8")
 
+def decryptJWTToken(jwtchiffre):
+    f = open('mykey.pem','r')
+    key = RSA.import_key(f.read())
+    encoded_chiffre_jwt=""
+    for i in range(len(jwtchiffre)):
+        #print(jwtchiffre[i][0],key,jwtchiffre[i][1],jwtchiffre[i][2],jwtchiffre[i][3])
+        jwtdechiffre=decrypt(jwtchiffre[i][0],key,jwtchiffre[i][1],jwtchiffre[i][2],jwtchiffre[i][3])
+        if(i!=len(jwtchiffre)-1):
+            encoded_chiffre_jwt+=str(jwtdechiffre)+"."
+        else:
+            encoded_chiffre_jwt+=str(jwtdechiffre)
+
+    return encoded_chiffre_jwt
+
+
 context = zmq.Context()
 send_socket = context.socket(zmq.PUSH)
-send_socket.connect('tcp://127.0.0.1:5557')
+send_socket.connect('tcp://127.0.0.1:5577')
 
 def print_incoming_messages():
     recv_socket = context.socket(zmq.PULL)
-    recv_socket.connect('tcp://127.0.0.1:5556')
+    recv_socket.connect('tcp://127.0.0.1:5576')
     while True:
         msg = recv_socket.recv_string()
-        print(f'Message from server: {msg}')
+        #print(msg)
+        msgsplited=msg.split("***")
+        newmsg=[]
+        msgtab=[]
+        msgsplit=[]
+        for i in range(len(msgsplited)-1):
+            msgsplit.append(msgsplited[i].split("^^^"))
+        for x in range(len(msgsplit)):
+            del msgsplit[x][-1]
+        
+        for i in range(len(msgsplit)):
+            for j in range(len(msgsplit[i])):
+                msgsplit[i][j]=msgsplit[i][j].encode('ISO-8859-1')
+        encoded_chiffre_jwt=decryptJWTToken(msgsplit)
+        print("\n",encoded_chiffre_jwt)
+        
+
 
 recv_thread = threading.Thread(target=print_incoming_messages)
 recv_thread.start()
