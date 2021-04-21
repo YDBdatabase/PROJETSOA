3bc31c73dd5a6c9392af60d95784412b082f88e9
Bigest <bigest28@gmail.com>
2021-04-21 20:52:52 +0200

    modif

--- jwt/ZMQtestClient.py
+++ jwt/ZMQtestClient.py
@@ -31,11 +31,11 @@ def decryptJWTToken(jwtchiffre):
 
 context = zmq.Context()
 send_socket = context.socket(zmq.PUSH)
-send_socket.connect('tcp://127.0.0.1:5579')
+send_socket.connect('tcp://127.0.0.1:5581')
 
 def print_incoming_messages():
     recv_socket = context.socket(zmq.PULL)
-    recv_socket.connect('tcp://127.0.0.1:5578')
+    recv_socket.connect('tcp://127.0.0.1:5580')
     while True:
         msg = recv_socket.recv_string()
         #print(msg)
